c8ae058d0ad9e52859dc1e51e21145fd0f4b8e49
magdahime <magdalena.majkowska1@gmail.com>
2020-03-27 19:13:03 +0100

    Graphing done

--- driver.py
+++ driver.py
@@ -11,10 +11,19 @@ def main():
     for line in open("scripts/urls.txt"):
         line = line[:-1]
         data_frames[gd.get_name(line)] = gd.retrieve(line, gd.get_name(line), "raw_data")
-    #FORMATTING AND GRAPHING
+    #GETTING KEYS
     keys_list = data_frames.keys()
-    fd.format_corona((data_frames[gd.get_key(keys_list, "coronacases")], data_frames[gd.get_key(keys_list, "coronadeaths")]))
-    fd.format_sars(data_frames[gd.get_key(keys_list, "sars")])
-    fd.format_swine_flu(data_frames[gd.get_key(keys_list, "swine")])
+    key_corona1 = gd.get_key(keys_list, "coronacases")
+    key_corona2 = gd.get_key(keys_list, "coronadeaths")
+    key_swine = gd.get_key(keys_list, "swine")
+    key_sars = gd.get_key(keys_list, "sars")
+    #GRAPHING FOR EACH DISEASE
+    #fd.format_corona((data_frames[key_corona1], data_frames[key_corona2]))
+    #fd.format_sars(data_frames[key_sars])
+    #fd.format_swine_flu(data_frames[key_swine])
+    #GRAPHING TOGETHER
+    fd.gather_data(((data_frames[key_corona1], data_frames[key_corona2])),
+                   data_frames[key_sars],data_frames[key_swine])
+    
 
 main()
--- scripts/formatting_data.py
+++ scripts/formatting_data.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
+
 def get_rows(data_frame, top10):
-    
+
     cases_list =[]
     for region in top10:
         new_data_frame = data_frame.loc[data_frame["Country/Region"]==region]
@@ -13,10 +14,12 @@ def get_rows(data_frame, top10):
     
     return cases_list
 
+
 def get_dates(data_frame):
-    
+
     return data_frame.columns.tolist()[1:]
-    
+
+
 def draw_graph(plots, title, disease, ticks):
     x_axis, x_axis_name, y1_axis, y2_axis, y_axis_name = plots
     plt.style.use('seaborn')
@@ -44,29 +47,47 @@ def get_top10(data_frame, parameter, dates):
     data_frame = data_frame.sort_values(by = [dates, parameter], ascending=False)
     return data_frame["Country"].tolist()[:10]
 
-def format_data(data_frame, region, disease, dates, cases, deaths, ticks):
+
+def format_data(data_frame, region, disease, str_dates, str_cases, str_deaths):
     
     data_frame = data_frame.loc[data_frame["Country"]==region]
-    plots = (data_frame[dates],"Dates", data_frame[cases],
-               data_frame[deaths], "Cases and deaths")
-    draw_graph(plots, f"Cases of {disease} in {region}", disease, ticks)
+    cases = data_frame[str_cases]
+    deaths = data_frame[str_deaths]
+    dates = data_frame[str_dates]
+    return (cases, deaths, dates, disease)
+    
+def format_sars(data_frame):
+
+    str_cases = "Cumulative number of case(s)"
+    str_dates = "Date"
+    top10 = get_top10(data_frame, str_cases, str_dates)
+    for region in top10:
+        graph_sars(data_frame, region, str_cases, str_dates)
+
 
 def format_swine_flu(data_frame):
-    cases = "Cases"
-    dates = "Update Time"
-    top10 = get_top10(data_frame, cases, dates)
+
+    str_cases = "Cases"
+    str_dates = "Update Time"
+    top10 = get_top10(data_frame, str_cases, str_dates)
     for region in top10:
-        format_data(data_frame, region, "Swine flu", dates,
-                    cases, "Deaths", 2)
+        graph_swine_flu(data_frame, region, str_cases, str_dates)
 
 
-def format_sars(data_frame):
-    cases = "Cumulative number of case(s)"
-    dates = "Date"
-    top10 = get_top10(data_frame, cases, dates)
-    for region in top10:  
-        format_data(data_frame, region, "SARS", dates,
-                    cases, "Number of deaths", 7)
+def graph_swine_flu(data_frame, region, str_cases, str_dates):
+ 
+    cases, deaths, dates, disease = format_data(data_frame, region, "Swine flu", str_dates,
+                                                str_cases, "Deaths")
+    plots = (dates, "Dates", cases, deaths, "Cases and deaths")
+    draw_graph(plots, f"Cases of Swine flu in {region}", disease, 2)
+
+
+
+def graph_sars(data_frame, region, str_cases, str_dates):
+
+    cases, deaths, dates, disease = format_data(data_frame, region, "SARS", str_dates, str_cases, "Number of deaths")
+    plots = (dates, "Dates", cases, deaths, "Cases and deaths")
+    draw_graph(plots, f"Cases of SARS in {region}", disease, 7)
 
 
 def format_corona(data_frames):
@@ -82,3 +103,68 @@ def format_corona(data_frames):
         plots = ((dates, "Dates", cases[country],
                deaths[country], "Cases and deaths"))
         draw_graph(plots, f"Cases of COVID-19 in {top10[country]}","COVID-19",7)
+
+def unpack(list1, list2, value1, value2):
+    list3 = []
+    for x in range(len(list1)):
+        list3.append((list1[x], list2[x], value1, value2))
+    return list3
+
+
+def gather_data(dfcorona, dfsars, dfswine):
+    dfcorona_cases, dfcorona_deaths = dfcorona
+    SARS = []
+    SWINE = []
+    dates = get_dates(dfcorona_cases)
+    last_date = dates[-1]
+    top10 = dfcorona_cases.sort_values(by = last_date, ascending = False)["Country/Region"].head(10).tolist()
+    for region in top10:
+        SARS.append(format_data(dfsars, region, "SARS", "Date", "Cumulative number of case(s)", "Number of deaths"))
+        SWINE.append(format_data(dfswine, region, "Swine flu", "Update Time", "Cases", "Deaths"))
+    cases = get_rows(dfcorona_cases, top10)
+    deaths = get_rows(dfcorona_deaths, top10)
+    CORONA = unpack(cases, deaths, dates, "COVID-19")
+    
+    for region in range(len(top10)):
+        format_together(SARS[region], SWINE[region], CORONA[region], top10[region])
+
+def graph_compare(x_ax, y1_ax, y2_ax, y3_ax, title):
+    x_axis, x_axis_name = x_ax
+    y1_values, descritption1 = y1_ax
+    y2_values, description2 = y2_ax
+    y3_values, description3 = y3_ax
+    plt.style.use('bmh')
+    fig = plt.figure(dpi=128, figsize=(20, 10))
+    plt.plot(x_axis, y1_values, c='purple', linewidth=2.0, label=descritption1)
+    plt.plot(x_axis, y2_values, c='green', linewidth=2.0, label=description2)
+    plt.plot(x_axis, y3_values, c='red', linewidth=2.0, label=description3)
+    plt.legend()
+    plt.title(title, fontsize=24)
+    plt.xlabel(x_axis_name, fontsize=20)
+    fig.autofmt_xdate()
+    plt.ylabel("", fontsize=20)
+    plt.tick_params(axis='both', which='major', labelsize=10)
+    save_filename = title
+    plt.savefig(f'output/compare/' + save_filename, bbox_inches='tight')
+
+
+
+def format_together(disease1, disease2, disease3, region):
+    cases1, deaths1, dates1, name1 = disease1
+    cases2, deaths2, dates2, name2 = disease2
+    cases3, deaths3, dates3, name3 = disease3
+    if len(cases1) == 0 or len(cases2) == 0 or len(cases3) == 0:
+        return
+    if len(deaths1) == 0 or len(deaths2) == 0 or len(deaths3) == 0:
+        return
+    else:
+        max_dates = min([len(dates1), len(dates2), len(dates3)])
+        dates = [x for x in range(1, max_dates+1)]
+        graph_compare((dates, "Days of pandemia"),
+                    (cases1[:max_dates], name1), (cases2[:max_dates], name2),
+                    (cases3[:max_dates], name3),
+                    f"Graph comparing development of three viral diseases in {region}")
+        graph_compare((dates, "Days of pandemia"),
+                    (deaths1[:max_dates], name1), (deaths2[:max_dates], name2),
+                    (deaths3[:max_dates], name3), 
+                    f"Graph comparing death rate of three viral diseases in {region}")
