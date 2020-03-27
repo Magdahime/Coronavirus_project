09badd8401cdadc75accdc8b1d009c1ec8daf16d
magdahime <magdalena.majkowska1@gmail.com>
2020-03-27 11:05:19 +0100

    New datasets improved graphing function

--- driver.py
+++ driver.py
@@ -9,8 +9,7 @@ def main():
     data_frames ={}
     for arg in sys.argv[1:]:
         data_frames[gd.get_name(arg)] = gd.retrieve(arg, gd.get_name(arg), "raw_data")
-    for data_frame in data_frames:
-       #fd.format_sars(data_frames[data_frame], "United States")
-       print(data_frames[data_frame].columns)
+        for df in data_frames:
+            print(data_frames[df])
 
 main()
--- scripts/formatting_data.py
+++ scripts/formatting_data.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def draw_graph(plots, title):
+def draw_graph(plots, title, disease, ticks):
     x_axis, x_axis_name, y1_axis, y2_axis, y_axis_name = plots
     plt.style.use('seaborn')
     fig = plt.figure(dpi=128, figsize=(20, 10))
@@ -15,30 +15,42 @@ def draw_graph(plots, title):
     #SHOWNG ONLY THE 7TH LABEL
     frame1 = plt.gca()
     labels = [label.set_visible(False) for label in frame1.axes.get_xticklabels()]
-    labels = [label.set_visible(True) for label in frame1.axes.get_xticklabels()[::7]]
+    labels = [label.set_visible(True) for label in frame1.axes.get_xticklabels()[::ticks]]
     plt.title(title, fontsize=24)
     plt.xlabel(x_axis_name, fontsize=20)
     fig.autofmt_xdate()
     plt.ylabel("", fontsize=20)
+    plt.yscale(value="log")
     plt.tick_params(axis='both', which='major', labelsize=10)
     save_filename = title
-    plt.savefig('output/SARS/' + save_filename, bbox_inches='tight')
+    plt.savefig(f'output/{disease}/' + save_filename, bbox_inches='tight')
 
 
-def get_top10(data_frame, parameter):
-    data_frame = data_frame.sort_values(by = ["Date", parameter], ascending=False)
+def get_top10(data_frame, parameter, dates):
+    data_frame = data_frame.sort_values(by = [dates, parameter], ascending=False)
     return data_frame["Country"].tolist()[:10]
 
-def format_sars(data_frame, region):
-    print("Countries with most cases of SARS:")
-    cases = "Cumulative number of case(s)"
-    top10 = get_top10(data_frame, cases)
-    for country in top10:
-        print(f"- {country}")
+def format_data(data_frame, region, disease, dates, cases, deaths, ticks):
+    
     data_frame = data_frame.loc[data_frame["Country"]==region]
-    plots = (data_frame["Date"],"Dates", data_frame[cases],
-               data_frame["Number of deaths"], "Cases and deaths")
-    draw_graph(plots, f"Cases of SARS in {region}")
+    plots = (data_frame[dates],"Dates", data_frame[cases],
+               data_frame[deaths], "Cases and deaths")
+    draw_graph(plots, f"Cases of {disease} in {region}", disease, ticks)
+
+def format_swine_flu(data_frame):
+    cases = "Cases"
+    dates = "Update Time"
+    top10 = get_top10(data_frame, cases, dates)
+    for region in top10:
+        format_data(data_frame, region, "Swine flu", dates,
+                    cases, "Deaths", 2)
 
 
-def format_mers(data_frame):
+def format_sars(data_frame):
+    cases = "Cumulative number of case(s)"
+    dates = "Date"
+    top10 = get_top10(data_frame, cases, dates)
+    for region in top10:  
+        format_data(data_frame, region, "SARS", dates,
+                    cases, "Number of deaths", 7)
+
